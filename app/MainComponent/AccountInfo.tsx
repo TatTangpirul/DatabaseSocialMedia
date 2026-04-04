b479d1ac0fa3cc62aaade1863eeec491376a5253
TatTangpirul <00462@kvis.ac.th>
2026-04-05 05:05:27 +0800

    First Commit: Basic UI

---
+++ app/MainComponent/AccountInfo.tsx
@@ -0,0 +1,8 @@
+// app/components/AccountInfo.tsx
+export default function AccountInfo() {
+  return (
+    <div className="w-64 bg-white p-4 rounded shadow-lg mt-16">
+      {/* Account info content will go here */}
+    </div>
+  );
+}
---
+++ app/MainComponent/Feed.tsx
@@ -0,0 +1,8 @@
+// app/components/AccountInfo.tsx
+export default function Feed() {
+  return (
+    <div className="w-64 bg-white p-4 rounded shadow-lg mt-16">
+      {/* Account info content will go here */}
+    </div>
+  );
+}
